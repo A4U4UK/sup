7969c75d6f2023276109c78adba2c94403044755
A4U4UK <beket9583@gmail.com>
2023-12-10 18:46:22 +0600

    first commit

---
+++ components/preview.vue
@@ -0,0 +1,120 @@
+<script setup>
+const user = useSupabaseUser();
+</script>
+
+<template>
+  <div class="header">
+    <div class="wrap">
+      <div class="header_top">
+        <div class="logo">
+          <a href="index.html"><img src="web/images/logo.png" alt="" /></a>
+        </div>
+        <div class="clear"></div>
+      </div>
+      <div class="navigation">
+        <a class="toggleMenu" href="#">Menu</a>
+        <ul class="nav">
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li class="test">
+            <a href="#">Appliances</a>
+          </li>
+          <li>
+            <a href="#">Computers</a>
+          </li>
+          <li>
+            <a href="#">Health & Beauty</a>
+          </li>
+          <li>
+            <a href="#">Home & Garden</a>
+          </li>
+          <li>
+            <a href="#">Office Supplies</a>
+          </li>
+          <li>
+            <a href="#">Sports Equipment</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+        <span class="left-ribbon"> </span>
+        <span class="right-ribbon"> </span>
+      </div>
+    </div>
+  </div>
+  <div class="main">
+    <div class="wrap">
+      <div class="preview-page">
+        <div class="section group">
+          <div class="cont-desc span_1_of_2">
+            <div class="product-details">
+              <div class="desc span_3_of_2">
+                <h2>Whirlpool LTE5243D 3.4 CuFt....</h2>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged.
+                </p>
+                <div class="price">
+                  <p>Price: <span>$839.93</span></p>
+                </div>
+                <div class="available">
+                  <ul>
+                    <li><span>Shipping Weight:</span>&nbsp; 75.58 kg</li>
+                  </ul>
+                </div>
+                <div class="share-desc">
+                  <div class="share">
+                    <p>Number of units :</p>
+                  </div>
+                  <div class="clear"></div>
+                </div>
+              </div>
+              <div class="clear"></div>
+            </div>
+            <div class="product_desc">
+              <div id="horizontalTab">
+                <div class="product-tags">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="wrap">
+      <div class="copy_right">
+        <p>
+          Copy rights (c). All rights Reseverd | Template by
+          <a href="http://w3layouts.com" target="_blank">W3Layouts</a>
+        </p>
+      </div>
+      <div class="footer-nav">
+        <ul>
+          <li><a href="#">Terms of Use</a> :</li>
+          <li><a href="#">Privacy Policy</a> :</li>
+          <li><a href="#">Contact Us</a> :</li>
+          <li><a href="#">Sitemap</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
